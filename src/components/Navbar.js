8254f655b8703b676fafb87a535a07be4c0c4061
Arunabho Bhattacharya <barunabho@gmail.com>
2024-05-23 13:17:00 +0530

    youtube logo and hamburger logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsYoutube } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <div>
-        
+    <div className='flex justify-between px-14 h-14 items-center bg-[#212121] opacity-95'>
+        <div className='flex gap-8 items-center text-2x1 text-white'>
+            <div>
+                <GiHamburgerMenu/>
+            </div>
+            <div className='flex gap-2 items-center justify-center'>
+                <BsYoutube className='text3x1 text-red-500'/>
+                <span className='font-bold text-2xl'>Youtube </span>
+            </div>
+            
+        </div>
     </div>
   )
 }
