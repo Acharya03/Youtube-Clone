5d1e46c591e7e5c88f8982800a078831d6c7a7cd
Arunabho Bhattacharya <barunabho@gmail.com>
2024-05-23 13:34:49 +0530

    added search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BsYoutube } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMicrophone } from "react-icons/fa";
+
 
 export default function Navbar() {
   return (
@@ -13,7 +16,23 @@ export default function Navbar() {
                 <BsYoutube className='text3x1 text-red-500'/>
                 <span className='font-bold text-2xl'>Youtube </span>
             </div>
-            
+            <div className='flex items-center justify-center gap-5'>
+              <form>
+                <div className='flex bg-zinc-900 items-center h-10 px-4 pr-0 rounded-3xl'>
+                  <div className='flex gap-5 items-center pr-5'>
+                    <input type='text' placeholder='search' 
+                    className='w-96 bg-zinc-900 focus: outline-none border-none'/>
+                  </div>
+                  <button className='h-10 w-16 flex items-center justify-center
+                   bg-zinc-800 rounded-r-3xl'>
+                    <AiOutlineSearch className='f'/>
+                  </button>
+                </div>
+              </form>
+              <div className='text-x1 p-3 bg-zinc-900 rounded-full'>
+                <FaMicrophone />
+              </div>
+            </div>
         </div>
     </div>
   )
