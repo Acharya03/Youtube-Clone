20743b1e1ac2e841e6994eb05f36caf83160716a
Arunabho Bhattacharya <barunabho@gmail.com>
2024-05-23 19:59:56 +0530

    sidebar otherLink update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -33,12 +33,28 @@ export default function Sidebar() {
     ]
   return (
     <div className='w-2/12 bg-[#212121] pr-5 overflow-auto pb-8 h-screen'>
-        <ul className='flex flex-col border-b-1 border-gray-800'>
+        <ul className='flex flex-col border-b-2'>
             {mainLinks.map(
                 ({icon,name}) => {
                     return(
                         <li key={name} className={`pl-6 py-2 hover:bg-zinc-500 ${name === "Home" ? "bg-slate-600" : " "}`}>
-                             <a href="#" className='flex items-center'>
+                             <a href='#' className='flex items-center gap-5'>
+                                {icon}
+                             </a>
+                             <span className='text-sm tracking-wider'>
+                                  {name}
+                            </span>
+                        </li>
+                    )
+                }
+            )}
+        </ul>
+        <ul className='flex flex-col border-b-2'>
+            {otherLinks.map(
+                ({icon,name}) => {
+                    return(
+                        <li key={name} className={`pl-6 py-2 hover:bg-zinc-500 ${name === "Home" ? "bg-slate-600" : " "}`}>
+                             <a href='#' className='flex items-center'>
                                 {icon}
                              </a>
                              <span>
