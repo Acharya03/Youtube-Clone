87167538205213160de9aa6eeaf65b17d59c43ae
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-02 20:14:59 +0530

    duration parts

--- src/utils/parseVideoDuration.js
+++ src/utils/parseVideoDuration.js
@@ -1,4 +1,4 @@
-export parseVideoDuration = (duration) => {
+export const parseVideoDuration = (duration) => {
 
     console.log(duration);
 
@@ -17,10 +17,21 @@ export parseVideoDuration = (duration) => {
         return `${durationParts[0]}:${parseInt(durationParts[1])<10 ? `0${durationParts[1]}` 
         :durationParts[1]
     }:${
-
+        parseInt(durationParts[2])<10 ? `0${durationParts[2]}` : durationParts[2]
     }`;
     }
 
+    if(durationParts.length === 2){
+        return `${durationParts[0]}:${parseInt(durationParts[1])<10 ? `0${durationParts[1]}` 
+        :durationParts[1]
+    }`;
+    }
+
+    if(durationParts.length === 1){
+        return `$0:${parseInt(durationParts[0])<10 ? `0${durationParts[0]}` 
+        :durationParts[0]
+    }`;
+    }
     return (
     
 <>
